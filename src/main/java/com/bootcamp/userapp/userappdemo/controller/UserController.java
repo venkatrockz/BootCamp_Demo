7bcf774ef40f94595d6d737d6214a3b01f0f7744
venkatrockz <venkateshkandula5@gmail.com>
2018-02-16 17:19:36 -0600

    test commit from github client

--- src/main/java/com/bootcamp/userapp/userappdemo/controller/UserController.java
+++ src/main/java/com/bootcamp/userapp/userappdemo/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @Controller
 public class UserController
